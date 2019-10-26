3db86ef005b1e1793c4a7502a5010a82910d5aec
Mayram <sultanovamayram4@gmail.com>
2019-10-26 08:55:34 +0600

    11b commit

---
+++ task11.2.2.py
@@ -0,0 +1,16 @@
+s = input("Enter the sentence : ")
+n = 2
+print(s[3], )  # In the first line, print the third character of this string.
+print(s[0:-1])  # In the second line, print the second to last character of this string.
+print(s[0:5])  # In the third line, print the first five characters of this string.
+print(
+    s[-2]
+)  # In the fourth line, print all but the last two characters of this string.
+print(s[::2])  # print all the characters of this string with even indices
+print(s[1::2])  # print all the characters of this string with odd indices
+print(s[::-1])  # line, print all the characters of the string in reverse order.
+print(
+    s[: -n - 1 : -1]
+)  # print every second character of the string in reverse order,starting from the last one
+print (len(s))
+ #not finished last
